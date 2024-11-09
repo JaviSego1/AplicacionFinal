19c9b2534b3952cdfba08473ab98d411c62d1028
JaviSego1 <javiersegoviamartinez@gmail.com>
2024-11-09 19:47:34 +0100

    Commit Login y Cardview

--- app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.aplicacionfinal
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
